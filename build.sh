38a28a3525bfd3c7bb067c13e140e560951e2d3b
Ben Davis <bendavis493@gmail.com>
2024-12-05 09:55:03 -0700

    updated build file

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python ProfessorAI/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python ProfessorAI/manage.py migrate
